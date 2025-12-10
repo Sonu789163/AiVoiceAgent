80c5890bd0275c23b374cd1497c6c177f76c130a
Sonu789163 <sonuv@excollo.com>
2025-12-10 19:18:55 +0530

    version update

--- frontend/src/components/StudentDetails.jsx
+++ frontend/src/components/StudentDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import '../StudentDetails.css';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+const API_URL = import.meta.env.VITE_API_URL ;
 
 export default function StudentDetails() {
     const [students, setStudents] = useState([]);
